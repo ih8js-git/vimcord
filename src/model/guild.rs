fd04275707d23c44b2f42a257d71934a6de5ec0e
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-14 15:47:49 +0100

    [UPD] guild.rs

--- src/model/guild.rs
+++ src/model/guild.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Guild {
     pub id: String,
     pub name: String,
