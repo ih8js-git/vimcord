d21416b90ef91a316a65d318fbbe4c6f2d4ca0fb
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-02-26 17:18:17 +0100

    [UPDATE] dm.rs
    Added group DMs names

--- src/api/dm.rs
+++ src/api/dm.rs
@@ -9,14 +9,21 @@ pub struct DM {
     pub channel_type: u8,
     pub last_message_id: Option<String>,
     pub recipients: Vec<User>,
+    pub name: Option<String>,
 }
 
 impl DM {
     pub fn get_name(&self) -> String {
-        self.recipients
+        let users = self
+            .recipients
             .iter()
             .map(|u| u.username.clone())
             .collect::<Vec<String>>()
-            .join(", ")
+            .join(", ");
+        if let Some(name) = self.name.clone() {
+            format!("{name} ({users})")
+        } else {
+            users
+        }
     }
 }
